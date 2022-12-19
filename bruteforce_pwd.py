b7e721573c31595cf199a8ee9aa1f3afc050a7d0
sreesindhusruthiyadavalli <sssindh@gmail.com>
2022-12-19 16:32:05 +0530

    Add randomSet in python

---
+++ bruteforce_pwd.py
@@ -0,0 +1,38 @@
+import zipfile
+import itertools
+import time
+
+# Function for extracting zip files to test if the password works!
+def extractFile(zip_file, password):
+    try:
+        zip_file.extractall(path='/tmp', pwd=password.encode())
+        return True
+    except KeyboardInterrupt:
+        exit(0)
+    except Exception as e:
+        pass
+
+# Main code starts here...
+# The file name of the zip file.
+zipfilename = '/tmp/alien-zip-2092.zip'
+alphabet = '1234567890'
+zip_file = zipfile.ZipFile(zipfilename)
+
+# We know they always have 3 characters
+# For every possible combination of 3 letters from alphabet...
+for c in itertools.product(alphabet, repeat=3):
+    # Add the three letters to the first half of the password.
+    password = ''.join(c)
+    # Try to extract the file.
+    print("Trying: %s" % password)
+    # If the file was extracted, you found the right password.
+    if extractFile(zip_file, password):
+        print('*' * 20)
+        print('Password found: %s' % password)
+        print('Files extracted...')
+        exit(0)
+
+# If no password was found by the end, let us know!
+print('Password not found.')
+
+
---
+++ randomSet.py
@@ -0,0 +1,48 @@
+class Set():
+    def __init__(self):
+        self.dst = {}
+        self.indexDst = {}
+        self.lst = []
+        return 
+
+    def insert(self, val):
+        if val in self.dst:
+            return False
+        self.dst[val] = True
+        self.lst.append(val)
+        self.indexDst[val] = len(self.lst) - 1
+        return True
+
+
+    def remove(self, val):
+        if val in self.dst:
+            del self.dst[val]
+            self.lst[self.indexDst[val]], self.lst[-1] = self.lst[-1], self.lst[self.indexDst[val]] 
+            #[1,6,3,4,5] {6:0}
+            self.lst.pop() 
+            self.indexDst[self.lst[self.indexDst[val]]] = self.indexDst[val]
+            del self.indexDst[val]
+            return True
+        return False
+
+
+    def giverandom(self):
+        length = len(self.dst.keys())
+        #randIndex = random.randint(0, length-1)
+        return self.lst[-1]
+        
+    def printtemp(self):
+        print(self.dst, self.indexDst, self.lst)
+        
+set = Set()
+set.insert(1)
+set.insert(6)
+set.insert(3)
+set.insert(4)
+set.insert(5)
+set.printtemp()
+set.remove(6)
+set.printtemp()
+print(set.giverandom())
+
+
